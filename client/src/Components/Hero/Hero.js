76d960df9e99b1cd9f96ebaf6f067f9930ad989a
Tricia Norris <tricianorris97@gmail.com>
2019-01-24 19:11:15 -0500

    committing to deploy

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import LazyHero from 'react-lazy-hero';
 
 class Hero extends Component {
-    render() {
+    render(props) {
         return (
             <div>
-                <LazyHero opacity="0.2" color="orange" minHeight="80vh" isCentered={true} imageSrc="http://orogoldstorelocator.com/wp-content/uploads/2016/08/OROGOLD-Popular-German-Outdoor-Activities-Cycling.jpg" parallaxOffset={2} >
+                <LazyHero opacity="0.2" color="orange" minHeight="70vh" isCentered={true} imageSrc='https://www.panoramaresort.com/assets/Tourism-Operators/images/_resampled/CroppedFocusedImageWzE4MDAsMTA4MCwieCIsMjE1XQ/1-stephennn-7394.JPG' parallaxOffset={2} >
                 <div className="hero-text">
                     <h1>{this.props.path}</h1>
                     <h1>RVA Activities</h1>
@@ -15,6 +15,8 @@ class Hero extends Component {
             </div>
         )}
 }
+
+// {require(`../../images/${this.props.name}.jpg`)}
 export default Hero;
 
 
