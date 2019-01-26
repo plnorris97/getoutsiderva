5640212cbaaa73e4fb5e0fefb0be0d189d36c80b
Tricia Norris <tricianorris97@gmail.com>
2019-01-26 15:46:57 -0500

    repushing database to heroku

--- client/src/Components/Auth/Signup.js
+++ client/src/Components/Auth/Signup.js
@@ -22,11 +22,25 @@ class Signup extends Component {
 		this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleChange = this.handleChange.bind(this)
 	}
-	handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
+	
+	handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value,
+        })
+        console.log(event.target.id)
+    }
+
+	checkSignup = (res) => {
+		API.checkAuth()
+			if (!res.data.err && (this.state.password === this.state.confirmPassword)) {
+				console.log('successful signup')
+				this.setState({ //redirect to login page
+					redirectTo: '/'
+				})
+			} else {
+				console.log('username already taken')
+			}  
+		}
 
 	handleSubmit = event => {
         event.preventDefault()
@@ -40,55 +54,11 @@ class Signup extends Component {
 				password: this.state.password,
 				confirmPassword: this.state.confirmPassword
 			})
-			console.log("created a user")
-            .then (res => {
-                console.log('login response: ')
-                console.log(res)
-				if (!res.data.err && (this.state.password === this.state.confirmPassword)) {
-					console.log('successful signup')
-					this.setState({ //redirect to login page
-						redirectTo: '/'
-					})
-				} else {
-					console.log('username already taken')
-				}  
-            })   
+			// console.log("created a user")
+            .then (res => this.checkSignup(res)) 
             .catch(err => console.log(err));
         }
     };
-
-
-	// handleSubmit(event) {
-	// 	event.preventDefault()
-	// 	console.log('sign-up handleSubmit, username: ')
-	// 	console.log(this.state.userName)
-
-	// 	//request to server to add a new username/password
-	// 	axios.post('/api/signup', {
-	// 		firstName: this.state.firstName,
-	// 		lastName: this.state.lastName,
-	// 		userName: this.state.userName,
-	// 		email: this.state.email,
-	// 		password: this.state.password,
-	// 		confirmPassword: this.state.confirmPassword
-	// 	})
-	// 		.then(response => {
-	// 			console.log(response)
-	// 			if (!response.data.errmsg && (this.state.password === this.state.confirmPassword)) {
-	// 				console.log('successful signup')
-	// 				this.setState({ //redirect to login page
-	// 					redirectTo: '/'
-	// 				})
-	// 			} else {
-	// 				console.log('username already taken')
-	// 			}
-	// 		}).catch(error => {
-	// 			console.log('signup error: ')
-	// 			console.log(error)
-
-	// 		})
-	// }
-
 
 render() {
 	if (this.state.redirectTo) {
@@ -117,7 +87,8 @@ render() {
                             />
 							 <MDBInput
                                 label="Last Name"
-                                icon="user"
+								icon="user"
+								id="lastName"
                                 group
                                 type="text"
                                 validate
@@ -128,7 +99,8 @@ render() {
                             />
 							<MDBInput
                                 label="User Name"
-                                icon="user"
+								icon="user"
+								id="userName"
                                 group
                                 type="text"
                                 validate
@@ -139,7 +111,8 @@ render() {
                             />
 							 <MDBInput
                                 label="Email"
-                                icon="envelope"
+								icon="envelope"
+								id="email"
                                 group
                                 type="text"
                                 validate
@@ -150,7 +123,8 @@ render() {
                             />
                             <MDBInput
                                 label="Password"
-                                icon="lock"
+								icon="lock"
+								id="password"
                                 group
                                 type="password"
                                 validate
@@ -159,113 +133,12 @@ render() {
                             />
                         </div>
                         <div className="text-center">
-                        <MDBBtn color="primary">Sign Up</MDBBtn>
+                        <MDBBtn color="primary" disabled={!this.state.firstName && this.state.lastName && this.state.userName && this.state.email && this.state.password} onClick={this.handleSubmit}>Sign Up</MDBBtn>
                         </div>
                     </form>
                     </MDBCol>
                 </MDBRow>
                 </MDBContainer>
-
-{/* 					
-					<Form className="form-horizontal">
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="firstname">First Name</Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									type="text"
-									id="firstName"
-									name="firstName"
-									placeholder="First Name"
-									value={this.state.firstName}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="lastName">Last Name</Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									type="text"
-									id="lastName"
-									name="lastName"
-									placeholder="Last Name"
-									value={this.state.lastName}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="userName">Username</Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									type="text"
-									id="userName"
-									name="userName"
-									placeholder="Username (ie.CrazyRunnerDude)"
-									value={this.state.userName}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="email">Email Address</Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									type="text"
-									id="email"
-									name="email"
-									placeholder="Email Address"
-									value={this.state.email}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="password">Password: </Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									placeholder="password"
-									type="password"
-									name="password"
-									value={this.state.password}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group">
-							<Col className="col-1 col-ml-auto">
-								<Label className="form-label" htmlFor="confirmPassword">Confirm Password: </Label>
-							</Col>
-							<Col className="col-3 col-mr-auto">
-								<Input className="form-input"
-									placeholder="Confirm Password"
-									type="confirmPassword"
-									name="confirmPassword"
-									value={this.state.confirmPassword}
-									onChange={this.handleChange}
-								/>
-							</Col>
-						</FormGroup>
-						<FormGroup className="form-group ">
-							<Col className="col-7">
-							<Button
-								className="btn btn-primary col-1 col-mr-auto"
-								onClick={this.handleSubmit}
-								type="submit"
-							>Sign up</Button>
-							</Col>
-						</FormGroup>
-					</Form> */}
 				</div>
 
 			)
