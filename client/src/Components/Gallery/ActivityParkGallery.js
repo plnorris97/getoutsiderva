2eac45cab05473391b41ff06a80c798963882f83
Jenny Liang <jennylove804@gmail.com>
2019-01-26 15:25:27 -0500

    park name

--- client/src/Components/Gallery/ActivityParkGallery.js
+++ client/src/Components/Gallery/ActivityParkGallery.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { MDBMask, MDBContainer, MDBView, MDBRow, } from "mdbreact";
-import "./style.css";
+
 import testingImg from '../../images/logo.PNG';
-import "./style.css";
 
 
-class HoverPage extends React.Component {
+
+class MaskPage extends React.Component {
   render() {
       const { name, img }= this.props
     return (
 
        <MDBContainer> 
         <MDBRow className="mt-4" style={{ padding:"1rem", margin:"1rem"}}>
-            <MDBView hover zoom>
+            <MDBView>
               <img
                 src= "https://mdbootstrap.com/img/Others/documentation/img%20(7)-mini.jpg"
                 className="img-fluid"
                 alt=""
               />
-              <MDBMask className="flex-center">
+              <MDBMask overlay="stylish-light" className="flex-center">
                 <h3 className="white-text"> {name} </h3>
               </MDBMask>
             </MDBView>
@@ -29,4 +29,4 @@ class HoverPage extends React.Component {
   }
 }
 
-export default HoverPage;
+export default MaskPage;
