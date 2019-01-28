f933df3d98f2a928c5fcf1542df49fc97127d94e
Tricia Norris <tricianorris97@gmail.com>
2019-01-28 17:28:35 -0500

    post route

--- Controllers/activitiesController.js
+++ Controllers/activitiesController.js
@@ -28,14 +28,14 @@ module.exports = {
       .find({name: parsedName})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  },
-  
-  // create: function(req, res) {
-  //   db.Book
-  //     .create(req.body)
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // },
+  },  
+  create: function(req, res) {
+    const parsedName = parseStr(req.params.name)
+    db.Activities
+      .create({name: parsedName})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
   // update: function(req, res) {
   //   db.Book
   //     .findOneAndUpdate({ _id: req.params.id }, req.body)
--- Routes/api/activities.js
+++ Routes/api/activities.js
@@ -4,10 +4,12 @@ const activityController = require("../../Controllers/activitiesController");
 // Matches with "/api/activities"
 router.route("/")
   .get(activityController.findAll)
+  .post(activityController.create)
 
 // Matches with "/api/activities/:name"
 router
   .route("/:name")
+  .put(activityController.update)
   .get(activityController.findByName)
 
 router
